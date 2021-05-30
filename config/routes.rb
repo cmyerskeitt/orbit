ae20ab05bdafd052ce522086e47c1fa9cc2745d0
github username <github email address>
2021-05-30 12:10:15 -0400

    Added HTML Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'html_pages/home'
+  get 'html_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
