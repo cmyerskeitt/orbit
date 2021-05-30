e3e1dac0d6c28cbb5a5805b621078ba4e0775cfc
github username <github email address>
2021-05-30 13:21:45 -0400

    add pretty testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'html_pages#home'
   get 'html_pages/help'
   get 'html_pages/about'
+  get 'html_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
