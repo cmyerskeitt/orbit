99924aff25025eed60c07b5b6ac9b31953aa44e6
github username <github email address>
2021-07-16 16:37:28 -0400

    Complete user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'html_pages#about'
   get '/contact', to: 'html_pages#contact'
   get '/signup', to: 'users#new'
+  resources :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
