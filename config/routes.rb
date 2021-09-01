2e7f17c27d5f5b8f36172ec40fb4680ca8c99b3d
github username <cmyerskeitt@gmail.com>
2021-08-31 22:42:42 -0400

    create session controller & update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/about', to: 'html_pages#about'
   get '/contact', to: 'html_pages#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'session#destroy'
   resources :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
