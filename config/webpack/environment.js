e93ce6eb266b1641736ba13c66bc9b4d079a48a2
github username <cmyerskeitt@gmail.com>
2021-09-02 23:59:42 -0400

    initial login

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepren('Provide',
+new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = environment
